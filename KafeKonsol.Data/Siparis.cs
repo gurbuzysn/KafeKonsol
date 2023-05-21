b172c57630bbd8fb950f8a95379800c63b1298d3
yasinn gürbüz <yasingurbuz1@hotmail.com>
2023-05-21 15:07:00 +0300

    Classes Created

---
+++ KafeKonsol.Data/Siparis.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KafeKonsol.Data
+{
+    public class Siparis
+    {
+        public int MasaNo { get; set; }
+        public SiparisDurum SiparisDurum { get; set; }
+        public decimal OdenenTutar { get; set; }
+        public DateTime? AcilisZamani { get; set; }
+        public DateTime? KapanisZamani { get; set; }
+        public List<SiparisDetay> SiparisDetaylar { get; set; }
+        public string ToplamTutarTl { get; }
+
+
+        public decimal ToplamTutar()
+        {
+
+        }
+
+
+    }
+}
